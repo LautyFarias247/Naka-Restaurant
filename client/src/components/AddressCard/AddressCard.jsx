03a609bcf850dcf4af77f4fab595014ad1753665
LautyFarías247 <lauty0618@gmail.com>
2023-06-14 04:08:34 -0300

    delete address button

--- client/src/components/AddressCard/AddressCard.jsx
+++ client/src/components/AddressCard/AddressCard.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 import style from './AddressCard.module.css'
 import { FaMapMarkerAlt } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const AddressCard = ({street, number, apartment, zipCode, neighborhood, locality, state}) => {
+	const handleDelete = () => {
+		Swal.fire({
+			title: `Seguro que deseas eliminar la dirección "${street} ${number}" ?`,
+			icon: 'info',
+			confirmButtonText: 'Eliminar',
+			showCancelButton: true,
+			cancelButtonText: "Cancelar",
+			iconColor: "#BF8D39"
+		}).then((result) => {
+			if (result.isConfirmed) {
+				Swal.fire({
+					title: `La dirección se borró correctamente`,
+					icon: 'success',
+					confirmButtonText: 'Ok',
+					showCancelButton: false,
+					iconColor: "#BF8D39"
+				}).then((result) => {
+					if(!result.isDenied) {
+						window.location.reload()
+					}
+				})
+			}
+		})
+	}
 	return (
 		<div className={style.container}>
 			<FaMapMarkerAlt className={style.icon}/>
@@ -11,7 +36,7 @@ const AddressCard = ({street, number, apartment, zipCode, neighborhood, locality
 				<span>{neighborhood}, {locality}, {state}</span>
 				<span>Código postal: {zipCode}</span>
 			</div>
-			<button className={style.boton}>Eliminar</button>
+			<button className={style.boton} onClick={handleDelete}>Eliminar</button>
 		</div>
 	)
 }
