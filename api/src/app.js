91af438bad6034820bc9a97ca863d3d1e0d1758d
LautyFarías247 <lauty0618@gmail.com>
2023-06-19 19:01:44 -0300

    cors domain

--- api/src/app.js
+++ api/src/app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://naka-restaurant.vercel.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
