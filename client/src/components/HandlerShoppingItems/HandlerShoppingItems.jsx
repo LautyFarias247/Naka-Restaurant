dc77638a0a83c5080a4fb117efaee5ae1b9875c5
LautyFarías247 <lauty0618@gmail.com>
2023-06-19 17:07:43 -0300

    verify logged user

--- client/src/components/HandlerShoppingItems/HandlerShoppingItems.jsx
+++ client/src/components/HandlerShoppingItems/HandlerShoppingItems.jsx
@@ -9,6 +9,7 @@ import {
 } from "../../redux/actions/actions";
 import Swal from "sweetalert2";
 import style from "./HandlerShopping.module.css";
+import { useNavigate } from "react-router-dom";
 
 const HandlerShoppingItems = ({ product, refresh, setRefresh }) => {
   const [aux, setAux] = useState(1);
@@ -18,50 +19,63 @@ const HandlerShoppingItems = ({ product, refresh, setRefresh }) => {
 
   const item = cart.find((item) => item._id === product._id);
   const dispatch = useDispatch();
-
+  const navigate = useNavigate();
   const handleAddFirstProduct = async () => {
-		if(!user.isActive){
-			return Swal.fire({
-				title: 'Lo sentimos...',
-				text: 'Tu cuenta se encuentra inhabilitada para realizar pedidos',
-				icon: 'error',
-				confirmButtonText: 'Aceptar',
-				iconColor: "#BF8D39",
-			})
-		}
+    if (!user.username) {
+      return Swal.fire({
+        title: "Iniciar sesiónn",
+        text: "Debes iniciar sesión para agregar productos a tus canasta",
+        icon: "info",
+        confirmButtonText: "Aceptar",
+        iconColor: "#BF8D39",
+      }).then(({ isConfirmed }) => {
+        if (isConfirmed) {
+          navigate("/account/login");
+        }
+      });
+    }
+    if (!user.isActive) {
+      return Swal.fire({
+        title: "Lo sentimos...",
+        text: "Tu cuenta se encuentra inhabilitada para realizar pedidos",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+        iconColor: "#BF8D39",
+      });
+    }
     await dispatch(addFirstProduct({ product }));
     setAux(aux + 1);
-		setRefresh(refresh +1)
+    setRefresh(refresh + 1);
   };
 
   const handleAddProduct = () => {
-		if(!user.isActive){
-			return Swal.fire({
-				title: 'Lo sentimos...',
-				text: 'Tu cuenta se encuentra inhabilitada para realizar pedidos',
-				icon: 'error',
-				confirmButtonText: 'Aceptar',
-				iconColor: "#BF8D39",
-			})
-		}
+    if (!user.isActive) {
+      return Swal.fire({
+        title: "Lo sentimos...",
+        text: "Tu cuenta se encuentra inhabilitada para realizar pedidos",
+        icon: "error",
+        confirmButtonText: "Aceptar",
+        iconColor: "#BF8D39",
+      });
+    }
     dispatch(addProduct(product));
     dispatch(saveCart({ userId: user._id, cart }));
     setAux(aux + 1);
-		setRefresh(refresh +1)
+    setRefresh(refresh + 1);
   };
 
   const handleRemoveProduct = () => {
     dispatch(removeProduct(product));
     dispatch(saveCart({ userId: user._id, cart }));
     setAux(aux + 1);
-		setRefresh(refresh +1)
+    setRefresh(refresh + 1);
   };
 
   const handleRemoveManyProducts = () => {
     dispatch(removeManyProducts(product));
     dispatch(saveCart({ userId: user._id, cart }));
     setAux(aux + 1);
-		setRefresh(refresh +1)
+    setRefresh(refresh + 1);
   };
 
   if (item?.quantity) {
